ec09b05ae03e5b97d7c58b418650a88ad4a26fce
Mihaela Ilieva <77100012+MihaelaIlieva@users.noreply.github.com>
2024-02-01 01:32:33 +0200

    Adding the whole project
    A web essay on CSS styles, classes and selectors

---
+++ javascript/scripts.js
@@ -0,0 +1,154 @@
+var counterOne=0;
+var counterTwo=0;
+var counterThree=0;
+var counterFour=0;
+var counterFive=0;
+var counterSix=0;
+var counterSeven=0;
+var counterEight=0;
+var counterNine=0;
+var counterTen=0;
+var counterEleven=0;
+function firstPictureAsCode(){
+    var ourParagraph=document.getElementById("imageOne");
+    if(counterOne%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterOne++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterOne++;
+    }   
+}
+function secondPictureAsCode(){
+    var ourParagraph=document.getElementById("imageTwo");
+    if(counterTwo%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterTwo++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterTwo++;
+    }
+}
+function thirdPictureAsCode(){
+    var ourParagraph=document.getElementById("imageThree");
+    if(counterThree%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterThree++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterThree++;
+    }
+}
+function fourthPictureAsCode(){
+    var ourParagraph=document.getElementById("imageFour");
+    if(counterFour%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterFour++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterFour++;
+    }
+}
+function fifthPictureAsCode(){
+    var ourParagraph=document.getElementById("imageFive");
+    if(counterFive%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterFive++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterFive++;
+    }
+}
+function sixthPictureAsCode(){
+    var ourParagraph=document.getElementById("imageSix");
+    if(counterSix%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterSix++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterSix++;
+    }
+}
+function seventhPictureAsCode(){
+    var ourParagraph=document.getElementById("imageSeven");
+    if(counterSeven%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterSeven++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterSeven++;
+    }
+}
+function eightPictureAsCode(){
+    var ourParagraph=document.getElementById("imageEight");
+    if(counterEight%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterEight++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterEight++;
+    }
+}
+function ninghtPictureAsCode(){
+    var ourParagraph=document.getElementById("imageNine");
+    if(counterNine%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterNine++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterNine++;
+    }
+}
+function tenthPictureAsCode(){
+    var ourParagraph=document.getElementById("imageTen");
+    if(counterTen%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterTen++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterTen++;
+    }
+}
+function eleventhPictureAsCode(){
+    var ourParagraph=document.getElementById("imageEleven");
+    if(counterEleven%2==0){
+        ourParagraph.style.visibility="visible";
+        ourParagraph.style.fontSize="100%";
+        counterEleven++;
+    }
+    else{
+        ourParagraph.style.visibility="hidden";
+        ourParagraph.style.fontSize="0";
+        counterEleven++;
+    }
+}
